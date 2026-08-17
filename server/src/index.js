5b7a5269e91f971e9a303c2e193c9da97affa017
viniciusdebruin <39091387+viniciusdebruin@users.noreply.github.com>
2026-08-16 23:08:19 -0300

    fix: import additional admin functions for enhanced functionality

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,9 @@
 import { createServer } from 'node:http'
 import { existsSync, readFileSync } from 'node:fs'
-import { isAbsolute, resolve } from 'node:path'
+import { isAbsolute, join, resolve } from 'node:path'
 import { loadEnvFile } from 'node:process'
 
+import { ensureSuperAdmin, handleAdminApi, handleAdminRecover, publicPack } from './admin.js'
 import { chapterProgressionWire, handleChapterRequest } from './chapters.js'
 import { handleCompatRequest } from './compat.js'
 import { loadRuntimeConfig, researchMode } from './config.js'
